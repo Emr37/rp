a650bd324e9c003eacb5014f6eac2d0654fc8931
Emrah Aksoy <emraksoy37@gmail.com>
2023-03-04 19:58:42 +0300

    fixed tables

--- src/components/Connected/index.tsx
+++ src/components/Connected/index.tsx
@@ -4,23 +4,17 @@ import styles from './styles.module.css'
 import base58 from "bs58";
 import { Keypair, LAMPORTS_PER_SOL, Transaction, SystemProgram } from '@solana/web3.js'
 import nextConfig from 'next.config';
+import { tables } from '../../pages/api/tables'
 
-//import { companyData } from '@/pages/api/companyData';
 import {
     useToast,
     Grid,
-    GridItem,
     Heading,
     Button,
     Text,
-    Badge,
-    Container,
     Box,
-    ButtonGroup,
-    Center,
     useColorModeValue,
     Stack,
-    Image,
     Flex,
     Input
 } from '@chakra-ui/react'
@@ -30,24 +24,12 @@ export const Connected: FC = () => {
 
     const [tx, setTx] = useState("");
     const [transactionState, setTransactionState] = useState(false);
-    const [balance, setBalance] = useState(0);
-    const [amount, setAmount] = useState(Number(0.05));
+
 
     const { connection } = useConnection();
     const { publicKey, sendTransaction } = useWallet();
 
     const companyPrivateKey = nextConfig.env.COM_PRIVATE_KEY;
-
-
-    useEffect(() => {
-        if (!connection || !publicKey) {
-            return;
-        }
-        connection.getBalance(publicKey).then((e) => {
-            if (!e) setBalance(0);
-            setBalance(e);
-        });
-    }, [connection, publicKey, tx]);
 
 
     const toast = useToast();
@@ -66,7 +48,6 @@ export const Connected: FC = () => {
 
         const payKeypair = Keypair.fromSecretKey(base58.decode(companyPrivateKey));
 
-
         if (!publicKey) {
             return
         }
@@ -83,6 +64,7 @@ export const Connected: FC = () => {
 
             const sign = await sendTransaction(transaction, connection);
             setTransactionState(true);
+
             const latestBlockHash = await connection.getLatestBlockhash();
 
             await connection.confirmTransaction({
@@ -112,380 +94,61 @@ export const Connected: FC = () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return (
-
-        <Flex 
-        bg={useColorModeValue('gray.100', 'gray.700')} 
-        justify={'center'}
+        <Flex
+            bg={useColorModeValue('gray.100', 'gray.700')}
+            justify={'center'}
         >
             <Grid templateColumns='repeat(3, 1fr)' gap={5} mt={10}>
-                
-                <Box
-                    role={'group'}                    
-                    w={'100%'}
-                    h={'50vh'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    
-                    >
-                    
-                    <Stack pt={10} align={'center'}>                       
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Table 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={600} fontSize={'md'}>
-                                Amount
-                            </Text>
-                            <Input 
-                            type='number'
-                            value={amount}
-                            onChange={(e) => setAmount(Number(e.target.value))}></Input>
-                        </Stack>
-                        <Button 
-                        color={'teal'} 
-                        variant={'ghost'}
-                        onClick={() => clickPay(companyPrivateKey, amount)}
-                        
-                        >
-                            Send Amount To Table
-                        </Button>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}                    
-                    w={'100%'}
-                    h={'50vh'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    
-                    >
-                    
-                    <Stack pt={10} align={'center'}>                       
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Table 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={600} fontSize={'md'}>
-                                Amount
-                            </Text>
-                            <Input></Input>
-                        </Stack>
-                        <Button color={'teal'} variant={'ghost'}>Send Amount To Table</Button>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}                    
-                    w={'30vw'}
-                    h={'50vh'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    
-                    >
-                    
-                    <Stack pt={10} align={'center'}>                       
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Table 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={600} fontSize={'md'}>
-                                Amount
-                            </Text>
-                            <Input></Input>
-                        </Stack>
-                        <Button color={'teal'} variant={'ghost'}>Send Amount To Table</Button>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}                    
-                    w={'100%'}
-                    h={'50vh'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>                       
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Table 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={600} fontSize={'md'}>
-                                Amount
-                            </Text>
-                            <Input></Input>
-                        </Stack>
-                        <Button color={'teal'} variant={'ghost'}>Send Amount To Table</Button>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}
 
-                    w={'100%'}
-                    h={'50vh'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>                       
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Table 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={600} fontSize={'md'}>
-                                Amount
-                            </Text>
-                            <Input></Input>
-                        </Stack>
-                        <Button color={'teal'} variant={'ghost'}>Send Amount To Table</Button>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}
-                    p={6}
-                    maxW={'330px'}
-                    w={'100%'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>
-                        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                            Masa 1
-                        </Text>
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Masa 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={800} fontSize={'xl'}>
-                                Tutar : 
-                            </Text>
-                            
-                        </Stack>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}
-                    p={6}
-                    maxW={'330px'}
-                    w={'100%'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>
-                        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                            Masa 1
-                        </Text>
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Masa 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={800} fontSize={'xl'}>
-                                Tutar : 
-                            </Text>
-                            
-                        </Stack>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}
-                    p={6}
-                    maxW={'330px'}
-                    w={'100%'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>
-                        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                            Masa 1
-                        </Text>
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Masa 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={800} fontSize={'xl'}>
-                                Tutar : 
-                            </Text>
-                            
-                        </Stack>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}
-                    p={6}
-                    maxW={'330px'}
-                    w={'100%'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>
-                        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                            Masa 1
-                        </Text>
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Masa 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={800} fontSize={'xl'}>
-                                Tutar : 
-                            </Text>
-                            
-                        </Stack>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}
-                    p={6}
-                    maxW={'330px'}
-                    w={'100%'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>
-                        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                            Masa 1
-                        </Text>
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Masa 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={800} fontSize={'xl'}>
-                                Tutar : 
-                            </Text>
-                            
-                        </Stack>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}
-                    p={6}
-                    maxW={'330px'}
-                    w={'100%'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>
-                        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                            Masa 1
-                        </Text>
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Masa 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={800} fontSize={'xl'}>
-                                Tutar : 
-                            </Text>
-                            
-                        </Stack>
-                    </Stack>
-                </Box>
-                <Box
-                    role={'group'}
-                    p={6}
-                    maxW={'330px'}
-                    w={'100%'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'lg'}
-                    pos={'relative'}
-                    zIndex={1}
-                    m={20}
-                    >
-                    
-                    <Stack pt={10} align={'center'}>
-                        <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-                            Masa 1
-                        </Text>
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                            Masa 1
-                        </Heading>
-                        <Stack direction={'row'} align={'center'}>
-                            <Text fontWeight={800} fontSize={'xl'}>
-                                Tutar : 
-                            </Text>
-                            
-                        </Stack>
-                    </Stack>
-                </Box>
-                
-        </Grid>
+                {
+                    tables.map((item, index) => {
+                        const [amount, setAmount] = useState('');
 
-</Flex>
+                        return (
+                            <Box key={index}
+                                role={'group'}
+                                w={'100%'}
+                                h={'50vh'}
+                                bg={useColorModeValue('white', 'gray.800')}
+                                boxShadow={'2xl'}
+                                rounded={'lg'}
+                                pos={'relative'}
+                                zIndex={1}
+                                py={5}
+                                px={3}
 
+                            >
+
+                                <Stack pt={10} align={'center'} justify={'center'}>
+                                    <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+                                        {item.name} - {item.id}
+                                    </Heading>
+                                    <Stack direction={'row'} align={'center'}>
+                                        <Text fontWeight={600} fontSize={'md'}>
+                                            Amount
+                                        </Text>
+                                        <Input
+
+                                            value={amount}
+                                            onChange={(e) => setAmount(e.target.value)} />
+                                    </Stack>
+                                    <Button
+                                        color={'teal'}
+                                        variant={'ghost'}
+                                        onClick={() => {
+                                            clickPay(companyPrivateKey, Number(amount));
+                                            setAmount('');
+                                        }}
+                                    >
+                                        Send Amount To Table
+                                    </Button>
+                                </Stack>
+                            </Box>
+                        )
+                    })
+                }
+            </Grid>
+        </Flex>
     )
-
-
 }
---
+++ src/pages/api/tables.ts
@@ -0,0 +1,8 @@
+export const tables = [
+    {id: 1, name: 'TABLE', price:''},
+    {id: 2, name: 'TABLE', price:''},
+    {id: 3, name: 'TABLE', price:''},
+    {id: 4, name: 'TABLE', price:''},
+    {id: 5, name: 'TABLE', price:''},
+    {id: 6, name: 'TABLE', price:''}
+]
