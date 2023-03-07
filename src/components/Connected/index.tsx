319a19683642c2e53551643140036c847f0ba4d5
Emrah Aksoy <emraksoy37@gmail.com>
2023-03-07 22:10:23 +0300

    new

--- src/components/Connected/index.tsx
+++ src/components/Connected/index.tsx
@@ -1,14 +1,11 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import React, { FC, useEffect, useState, useRef, useContext } from 'react'
-import styles from './styles.module.css'
+import React, { FC, useEffect, useState, useRef, MutableRefObject } from 'react'
 import base58 from "bs58";
 import { Keypair, LAMPORTS_PER_SOL, Transaction, SystemProgram } from '@solana/web3.js'
 import { encodeURL, createQR } from '@solana/pay'
 import nextConfig from 'next.config';
 import { tables } from '../../pages/api/tables'
 import BigNumber from 'bignumber.js';
-
-
 
 import {
     useToast,
@@ -20,14 +17,11 @@ import {
     useColorModeValue,
     Stack,
     Flex,
-    Input
+    Input    
 } from '@chakra-ui/react'
-import { useQr } from '@/context/QrContextProvider';
-
 
 
 export const Connected: FC = () => {
-    //const { qR, setQr, amount, tableId }: any = useQr();
 
     const [tx, setTx] = useState("");
     const [transactionState, setTransactionState] = useState(false);
@@ -102,59 +96,58 @@ export const Connected: FC = () => {
         }
     };
 
+    const qrRef:MutableRefObject<null> = useRef(null)
 
-    const listTables = tables.map((item, index) => {
+
+
+    const showQR = (amount: any, item: any) => {
+        if (amount <= 0) return resultToast('error', 'Amount is reqiured')
+        console.log('Show QR')   
+        const payKeypair = Keypair.fromSecretKey(base58.decode(companyPrivateKey));
+
+        item.url = encodeURL({
+            recipient: payKeypair!.publicKey,
+            amount: new BigNumber(amount),
+            reference: undefined,
+            label: `RPS: Table - ${item.id}`,
+            message: 'Thanks for your order',
+        });
+
+        console.log(item.url)
+        
+        const qrCode = createQR(item.url, 256, 'transparent');
+
+        console.log(qrCode)
+        console.log(qrRef.current)
+
+        if (qrRef.current) { 
+        qrCode.append(qrRef.current)
+
+        console.log(item.box)
+        console.log(qrRef)
+
+        } else return;
+    
+}
+
+    const listTables = tables.map((item, index, qrRef:any) => {
 
         const [amount, setAmount] = useState('');
         const [show, setShow] = useState(false);
-        const qrRef = useRef(null)
 
-        const showQR = (amount: any, item: any) => {
-
-
-            if (amount <= 0) {
-                resultToast('error', 'amount is required')
-            } else {
-                console.log('Show QR')
-
-
-                const payKeypair = Keypair.fromSecretKey(base58.decode(companyPrivateKey));
-
-                const url = encodeURL({
-                    recipient: payKeypair!.publicKey,
-                    amount: new BigNumber(amount),
-                    reference: undefined,
-                    label: `RPS: Table - ${item.id}`,
-                    message: 'Thanks for your order',
-                });
-                console.log(typeof url);
-                
-                const qrCode = createQR(url, 128, 'transparent');
-                console.log(qrCode);
-                console.log(qrRef.current);
-                if (qrRef.current) {
-                    qrCode.append(qrRef.current)
-                } else return;
-            }
-
-        }
-
-
+        
 
         return (
             <Box key={index}
                 role={'group'}
-                w={'300px'}
-                h={'400px'}
+                w={'450px'}
+                h={'600px'}
                 bg={useColorModeValue('white', 'gray.800')}
                 boxShadow={'2xl'}
                 rounded={'lg'}
                 py={5}
                 px={3}
-
             >
-
-
                 <Stack pt={10} align={'center'} justify={'center'} direction={'column'} >
                     <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
                         {item.name} - {item.id}
@@ -164,6 +157,7 @@ export const Connected: FC = () => {
                             Amount
                         </Text>
                         <Input
+                            defaultValue={0}
                             value={amount}
                             onChange={(e) => setAmount(e.target.value)} />
                     </Stack>
@@ -172,7 +166,6 @@ export const Connected: FC = () => {
                         variant={'solid'}
                         onClick={() => {
 
-                            showQR(amount, item)
                             setAmount('')
                         }}
                     >
@@ -185,14 +178,17 @@ export const Connected: FC = () => {
                         variant={'ghost'}
                         onClick={() => {
                             //clickPay(companyPrivateKey, Number(amount));
+                            showQR(amount, item)
                             setAmount('');
+
+
                         }}
                     >
                         Send Amount To QR
                     </Button>
-
-                    <Box bg={'gray.400'} h={'128px'} w={'128px'} ref={qrRef} />
-
+                    <Box bg={'gray.500'} w={256} h={256} id='qrId' ref={qrRef}/>
+                    
+                                        
                 </Stack>
             </Box>
         )
@@ -205,8 +201,7 @@ export const Connected: FC = () => {
             bg={useColorModeValue('gray.100', 'gray.700')}
             justify={'center'}
         >
-            <Grid templateColumns='repeat(3, 1fr)' gap={5} mt={10}>
-
+            <Grid templateColumns='repeat(3, 1fr)' gap={50} mt={10}>
                 {
                     listTables
                 }
--- src/pages/api/tables.ts
+++ src/pages/api/tables.ts
@@ -2,10 +2,10 @@ import nextConfig from "next.config";
 
 
 export const tables = [
-    {id: 1, name: 'TABLE', price:0.5},
-    {id: 2, name: 'TABLE', price:0.6},
-    {id: 3, name: 'TABLE', price:0.7},
-    {id: 4, name: 'TABLE', price:0.8},
-    {id: 5, name: 'TABLE', price:0.9},
-    {id: 6, name: 'TABLE', price:1}
+    {id: 1, name: 'TABLE', price:0.5, url:'', box:{}},
+    {id: 2, name: 'TABLE', price:0.6, url:'', box:{}},
+    {id: 3, name: 'TABLE', price:0.7, url:'', box:{}},
+    {id: 4, name: 'TABLE', price:0.8, url:'', box:{}},
+    {id: 5, name: 'TABLE', price:0.9, url:'', box:{}},
+    {id: 6, name: 'TABLE', price:1, url:'', box:{}}
 ]
